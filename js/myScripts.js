48471c1606b6b3a89f6787ae5ab717cd81c15dd3
Nikki Wolfe <nikki.taylor.wolfe@gmail.com>
2019-03-04 12:25:00 -0600

    Updated Home Page timing function
    Home Page timing function was switched to javascript based instead of liquid based due to it being a static site

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,3 +1,56 @@
+window.onload = function timeMessage() {
+    // ------------------------------------------------------
+    // home page message based on time
+    // ------------------------------------------------------
+    var time = new Date().getHours();
+    
+    if ((time == 0) || (time == 1) || (time == 2) || (time == 3) || (time == 4) || (time == 5)) {
+        document.getElementById("timeMessage").innerHTML = "sleeping away.";
+    } else if (time == 6) {
+        document.getElementById("timeMessage").innerHTML = "eating breakfast.";
+    } else if (time == 7) {
+        document.getElementById("timeMessage").innerHTML = "on a run.";
+    } else if (time == 8) {
+        document.getElementById("timeMessage").innerHTML = "meditating.";
+    } else if (time == 9) {
+        document.getElementById("timeMessage").innerHTML = "writing.";
+    } else if (time == 10) {
+        document.getElementById("timeMessage").innerHTML = "reading something or other.";
+    } else if (time == 11) {
+        document.getElementById("timeMessage").innerHTML = " working on a project.";
+    } else if (time == 12) {
+        document.getElementById("timeMessage").innerHTML = "eating some kind of lunch.";
+    } else if (time == 13) {
+        document.getElementById("timeMessage").innerHTML = "talking briefly on the phone.";
+    } else if (time == 14) {
+        document.getElementById("timeMessage").innerHTML = "developing a plan.";
+    } else if (time == 15) {
+        document.getElementById("timeMessage").innerHTML = "somehow managing.";
+    } else if (time == 16) {
+        document.getElementById("timeMessage").innerHTML = "writing.";
+    } else if (time == 17) {
+        document.getElementById("timeMessage").innerHTML = "solving some sort of problem.";
+    } else if (time == 18) {
+        document.getElementById("timeMessage").innerHTML = "eating dinner.";
+    } else if (time == 19) {
+        document.getElementById("timeMessage").innerHTML = "avoiding the Twitter.";
+    } else if (time == 20) {
+        document.getElementById("timeMessage").innerHTML = "washing dishes/doing nothing.";
+    } else if (time == 21) {
+        document.getElementById("timeMessage").innerHTML = "working on tomorrow.";
+    } else if (time == 22) {
+        document.getElementById("timeMessage").innerHTML = " reading a book.";
+    }
+    else if (time == 23) {
+        document.getElementById("timeMessage").innerHTML = "reading a magazine.";
+    }
+};
+
+
+
+
+
+
 $(document).ready(function () {
 
     // ------------------------------------------------------
@@ -75,7 +128,10 @@ $(document).ready(function () {
         $('.home-animation-wrapper').hide().css("display", "none");
     }
     // set the cookie
-    Cookies('visited', 'yes', { expires: 1, path: '/' });
+    Cookies('visited', 'yes', {
+        expires: 1,
+        path: '/'
+    });
 
 
 
@@ -90,8 +146,6 @@ $(document).ready(function () {
     // project image size fix
     // ------------------------------------------------------
     $(".single-project .project-content p img").parent().addClass("project-image");
-
-
 
 
 
